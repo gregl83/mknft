4da99166f01178062f615cf87b273b4ff002a4b7
gregl83 <general@gregorylanglais.com>
2021-11-30 07:23:47 -0500

    add struct docs

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,6 +4,7 @@ pub mod publish;
 
 use serde::{Deserialize, Serialize};
 
+/// Project attribute value representing PSD layers
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AttributeValue {
     name: String,
@@ -11,18 +12,28 @@ pub struct AttributeValue {
     weight: u32
 }
 
+/// Project attribute having collection of values or PSD layers
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Attribute {
     name: String,
     values: Vec<AttributeValue>
 }
 
+/// Config for `mknft` used when parsing PSD
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    name: String,
+    attributes: Vec<Attribute>
+}
+
+/// Project config prepared using `prepare` command
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ProjectConfig {
     name: String,
     attributes: Vec<Attribute>
 }
 
+/// Image (NFT) packaged using `package` command
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Image {
     name: String,
@@ -30,6 +41,7 @@ pub struct Image {
     path: String,
 }
 
+/// Package config packaged using `package` command
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PackageConfig {
     name: String,
