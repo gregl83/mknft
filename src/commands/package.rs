fa91153445a0a36d32110b92cdadca4426aa2ac6
gregl83 <general@gregorylanglais.com>
2021-11-30 22:09:36 -0500

    add attribute image exclude mechanism

--- src/commands/package.rs
+++ src/commands/package.rs
@@ -4,6 +4,7 @@ use std::io::BufReader;
 use std::fs;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::fmt::format;
 use std::hash::Hash;
 use sha2::{Sha256, Digest};
 use rand::prelude::*;
@@ -22,6 +23,16 @@ use crate::commands::Image;
 use crate::commands::ProjectConfig;
 use crate::commands::PackageConfig;
 
+// function to combine two layers of psd
+fn combine_layers(source : &DynamicImage, target: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
+    for (x, y, source_pixel) in source.pixels() {
+        let mut target_pixel = target.get_pixel_mut(x, y);
+        if source_pixel.0[3] > 0 {
+            target_pixel.0 = source_pixel.0;
+        }
+    }
+}
+
 pub async fn exec(matches: &ArgMatches<'_>) {
     let src = matches.value_of("src").unwrap();
     let dest = matches.value_of("dest").unwrap();
@@ -39,32 +50,19 @@ pub async fn exec(matches: &ArgMatches<'_>) {
     fs::create_dir(dest).unwrap();
     fs::create_dir(image_dest.clone()).unwrap();
 
-    // function to combine two layers of psd
-    fn combine_layers(source : &DynamicImage, target: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
-        for (x, y, source_pixel) in source.pixels() {
-            let mut target_pixel = target.get_pixel_mut(x, y);
-            if source_pixel.0[3] > 0 {
-                target_pixel.0 = source_pixel.0;
-            }
-        }
-    }
-
     let mut rng = thread_rng();
 
-    // fixme - support excludes
-    // detect exclusion
-
-    let mut layers: Vec<(Vec<(String, DynamicImage)>, WeightedIndex<u32>)> = vec![];
+    // create layer image buffers and weighted index for random selection
+    let mut layers: Vec<(Vec<DynamicImage>, WeightedIndex<u32>)> = vec![];
     for attribute in project_config.attributes.iter() {
         let mut index: Vec<u32> = vec![];
-        let mut images: Vec<(String, DynamicImage)> = vec![];
+        let mut images: Vec<DynamicImage> = vec![];
         for value in attribute.values.iter() {
             if let Some(value_path) = &value.path {
                 index.push(value.weight);
-                images.push((
-                    value.name.clone(),
+                images.push(
                     ImageReader::open(format!("{}/{}", src, value_path)).unwrap().decode().unwrap()
-                ));
+                );
             }
         }
         layers.push((
@@ -86,33 +84,54 @@ pub async fn exec(matches: &ArgMatches<'_>) {
         };
 
         let mut target: ImageBuffer::<Rgba<u8>, Vec<_>> = ImageBuffer::new(
-        layers[0].0[0].1.width(),
-        layers[0].0[0].1.height()
+        layers[0].0[0].width(),
+        layers[0].0[0].height()
         );
 
-        let mut image_id = String::new();
-        for (images, weights) in layers.iter() {
-            let index = weights.sample(&mut rng);
-            image_id = format!("{}{}", image_id, index);
-            let (name, image) = images.get(index).unwrap();
+        // set excludes tracking variables
+        let mut used: Vec<String> = vec![];
+        let mut excluded: HashSet<String> = HashSet::new();
+
+        // generate image from random weighted selection of layers
+        for (index, attribute) in project_config.attributes.iter().enumerate() {
+            let (images, weighted_index) = layers.get(index).unwrap();
+            let image_index = weighted_index.sample(&mut rng);
+            let image_config = attribute.values.get(image_index).unwrap();
+            for exclude in image_config.excludes.iter() {
+                for exclude_image in exclude.values.iter() {
+                    excluded.insert(format!("{}:{}", exclude.attribute_name.clone(), exclude_image.clone()));
+                }
+            }
+            used.push(format!("{}:{}", attribute.name.clone(), image_config.name.clone()));
+            let image = images.get(image_index).unwrap();
             combine_layers(&image, &mut target);
-            package_image.properties.push(name.clone());
+            package_image.properties.push(image_config.name.clone());
         }
 
-        // generate image hash for duplicate detection
-        let mut hasher = Sha256::new();
-        hasher.update(target.as_ref());
-        let hasher_result = hasher.finalize();
-        let hash = format!("{:X}", hasher_result);
+        // check for exclusion collision
+        let mut exclude_collision = false;
+        for u in used {
+            if excluded.contains(u.as_str()) {
+                exclude_collision = true;
+                break;
+            }
+        }
+        if !exclude_collision {
+            // generate image hash for duplicate detection
+            let mut hasher = Sha256::new();
+            hasher.update(target.as_ref());
+            let hasher_result = hasher.finalize();
+            let hash = format!("{:X}", hasher_result);
 
-        if !hashes.contains(hash.as_str()) {
-            sampled += 1;
-            let image_path = format!("{}/{}.png", image_dest.clone(), sampled);
-            target.save(image_path.clone()).unwrap();
-
-            hashes.insert(hash.to_owned());
-            package_image.path = image_path;
-            package_config.images.push(package_image);
+            // generate image if new/unique
+            if !hashes.contains(hash.as_str()) {
+                sampled += 1;
+                let image_path = format!("{}/{}.png", image_dest.clone(), sampled);
+                target.save(image_path.clone()).unwrap();
+                hashes.insert(hash.to_owned());
+                package_image.path = image_path;
+                package_config.images.push(package_image);
+            }
         }
     }
 
